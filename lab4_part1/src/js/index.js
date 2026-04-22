7b4b158810b6fd480f5f56a01ab683986ae7e61d
jacksonq-school <jacksonq@my.lanecc.edu>
2026-04-22 11:20:28 -0700

    Delete lab4_part1 directory

--- lab4_part1/src/js/index.js
+++
@@ -1,7 +0,0 @@
-import { TaskModel } from './model.js';
-import { TaskView } from './view.js';
-import { TaskController } from './controller.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  new TaskController(new TaskModel(), new TaskView());
-});
